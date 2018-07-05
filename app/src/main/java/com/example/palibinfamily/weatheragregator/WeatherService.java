4228467a4c2e82d306100311378a18855f0461dd
darkness <you@example.com>
2018-07-05 15:02:45 +0400

    service tipa work

--- app/src/main/java/com/example/palibinfamily/weatheragregator/WeatherService.java
+++ app/src/main/java/com/example/palibinfamily/weatheragregator/WeatherService.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.example.palibinfamily.weatheragregator.Presenter.MainActivityPresenter;
 import com.example.palibinfamily.weatheragregator.View.WeatherView;
 
+import java.util.concurrent.TimeUnit;
+
 public class WeatherService extends Service {
+    private static final String TAG = "WeatherService";
     private MainActivityPresenter servicePresenter = new MainActivityPresenter();
     private WeatherBinder binder = new WeatherBinder();
     private Preferences preferences;
@@ -34,6 +38,21 @@ public class WeatherService extends Service {
 
     public WeatherService() {
 //        preferences = new Preferences(getApplicationContext());
+        new Thread(new Runnable() {
+            public void run() {
+                for (int i = 1; i<=500; i++) {
+                    Log.d(TAG, " !!!!!!!!!!!!!! i = " + i);
+//                    showForegroundNotification("!!!!!!!!!!!!!!");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                stopSelf();
+            }
+        }).start();
     }
 
     @Nullable
