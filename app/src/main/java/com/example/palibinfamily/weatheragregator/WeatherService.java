876cc4244c197e35ab17d0768ee9bb2cdcdfb559
darkness <you@example.com>
2018-07-05 15:21:32 +0400

    service +test notifications(always)

--- app/src/main/java/com/example/palibinfamily/weatheragregator/WeatherService.java
+++ app/src/main/java/com/example/palibinfamily/weatheragregator/WeatherService.java
@@ -1,5 +1,7 @@
 package com.example.palibinfamily.weatheragregator;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
@@ -8,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.example.palibinfamily.weatheragregator.Presenter.MainActivityPresenter;
+import com.example.palibinfamily.weatheragregator.View.MainActivity.MainActivity;
 import com.example.palibinfamily.weatheragregator.View.WeatherView;
 
 import java.util.concurrent.TimeUnit;
@@ -42,7 +45,7 @@ public class WeatherService extends Service {
             public void run() {
                 for (int i = 1; i<=500; i++) {
                     Log.d(TAG, " !!!!!!!!!!!!!! i = " + i);
-//                    showForegroundNotification("!!!!!!!!!!!!!!");
+                    showForegroundNotification("ЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫы");
                     try {
                         TimeUnit.SECONDS.sleep(5);
                     } catch (InterruptedException e) {
@@ -59,5 +62,32 @@ public class WeatherService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
+    }
+
+
+    private static final int NOTIFICATION_ID = 1;
+
+    private void showForegroundNotification(String contentText) {
+        // Create intent that will bring our app to the front, as if it was tapped in the app
+        // launcher
+        Intent showTaskIntent = new Intent(getApplicationContext(), MainActivity.class);
+        showTaskIntent.setAction(Intent.ACTION_MAIN);
+        showTaskIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        showTaskIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(
+                getApplicationContext(),
+                0,
+                showTaskIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new Notification.Builder(getApplicationContext())
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(contentText)
+                .setSmallIcon(R.drawable.ic_weather_few_clouds)
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(contentIntent)
+                .build();
+        startForeground(NOTIFICATION_ID, notification);
     }
 }
